17a9c1cea6c9e54d4ad30b4a8a843ea26a55cf46
kamal <kamal.v3011f@gmail.com>
2026-05-02 22:03:50 +0530

    minor  changes

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,8 @@
 import js from '@eslint/js'
-import globals from 'globals'
+
 import reactHooks from 'eslint-plugin-react-hooks'
+import globals from 'globals'
+
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
